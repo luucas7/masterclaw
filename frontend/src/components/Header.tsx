f2d1a60027cef5e4833f364977dedf05e5ecd15e
Luucas7 <merlin.lucas99@gmail.com>
2024-04-13 23:15:58 +0200

    Header (non-responsive)

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -2,7 +2,7 @@ import useAuthUser from 'react-auth-kit/hooks/useAuthUser';
 import { Col, Row } from 'react-bootstrap';
 import {Link} from 'react-router-dom';
 
-
+import '../style/compiled/header.css';
 
 const Header = () => {
 
@@ -10,23 +10,32 @@ const Header = () => {
     <>
         <header>
             <Row>
-                <Col>
-                    <Col>
-                        <Link className='' to='/' ><li>Triconquest</li></Link>
-                    </Col>
-                    <Col>
-                        <Link className='' to='/adventure' ><li>Adventure</li></Link>
-                        <Link className='' to='/cards' ><li>Cards</li></Link>
 
-                    </Col>
-                        
-                    {!useAuthUser() && <Link className='' to='/login' ><li >Log in</li></Link>}      
+                <Col md={5} sm={5} className='flexbox flex-row justify-start'>
+                    <Link  to="/" >Triconquest</Link>
                 </Col>
+
+                <Col md={5} sm={5} className='flexbox flex-row justify-start'>
+                    <Link  to='/adventure' >Adventure</Link>
+                    <Link className='' to='/cards' >Cards</Link>
+                    <Link className='' to='/decks' >Decks</Link>    
+                </Col>
+
+                <Col md={2} sm={2} className='flexbox flex-row justify-end'>
+                    {!useAuthUser() ? <Link className='' to='/login' >Log in</Link>
+                    : <Link className='' to='/logout' >Log out</Link>}
+                </Col>
+
+
+
             </Row>
         </header>
     </>
 
   )
 }
+
+
+
 
 export default Header
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from './App';
-import './style/main.css'
+import './style/compiled/main.css'
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
