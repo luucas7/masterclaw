d2efbbf11e1442b322f58ecb97c36c644b14a0bc
Luucas7 <merlin.lucas99@gmail.com>
2024-06-09 00:25:39 +0200

    Updated regex patterns to fix security issues

--- frontend/src/ts/sanitizer.ts
+++ frontend/src/ts/sanitizer.ts
@@ -1,7 +1,6 @@
 const sanitize = (str: string) => str.replace(/[^a-zA-Z0-9]/g, '');
 
-const isEmail = (str: string) => /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(.\w{2,3})+$/.test(str) ? str : false;
+const isEmail = (str: string) => /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(str) ? str : false;
 
-const isUsername = (str: string) => /^[a-z][a-z]+\d*$|^[a-z]\d{2,}$/i.test(str) ? str.toLowerCase() : false;
-
+const isUsername = (str: string) => /^.{3,}$/.test(str) ? str.toLowerCase() : false;
 export { sanitize, isEmail, isUsername };
