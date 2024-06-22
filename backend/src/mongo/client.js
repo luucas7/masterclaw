cc1cbab905df3fae27d76420d981b51101725d82
Luucas7 <merlin.lucas99@gmail.com>
2024-06-22 23:57:28 +0200

    chore: Update Dockerfile for frontend to use Node.js version 20-alpine

--- backend/src/mongo/client.js
+++ backend/src/mongo/client.js
@@ -3,16 +3,16 @@ const mongoose = require('mongoose');
 require("dotenv").config('../.env');
 
 mongoose.connection.on('connected', () => {
-  console.log(new Date().toLocaleString(process.env.LOCALE), '- Connected to MongoDB');
+  console.log(new Date().toLocaleString('fr-FR'), '- Connected to MongoDB');
 });
 
 mongoose.connection.on('error', (err) => {
-  console.error(new Date().toLocaleString(process.env.LOCALE), '- Error connecting to MongoDB', err);
+  console.error(new Date().toLocaleString('fr-FR'), '- Error connecting to MongoDB', err);
   process.exit(1);
 });
 
 mongoose.connection.on('disconnected', () => {
-  console.log(new Date().toLocaleString(process.env.LOCALE), '- Disconnected from MongoDB');
+  console.log(new Date().toLocaleString('fr-FR'), '- Disconnected from MongoDB');
 });
 
 mongoose.connect(process.env.MONGO_URI);
